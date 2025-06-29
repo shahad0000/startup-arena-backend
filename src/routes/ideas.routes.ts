b6c5fd3298ec3b6f50aae372db092f139c8fdbbd
user1 <user01@em/com>
2025-06-29 19:52:16 +0300

    idea, votes, analatics done

--- src/routes/ideas.routes.ts
+++ src/routes/ideas.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { authorized } from '../middleware/auth.middleware';
-import { createIdea, deleteIdeaById, getAllIdeas, getIdeaById, getVotes, ideaDetails, postVote, updateIdeaById } from "../controllers/ideas.controller"
+import { createIdea, deleteIdeaById, getAllIdeas, getIdeaById, getVotes, ideaAnalatics, postVote, updateIdeaById } from "../controllers/ideas.controller"
 
 const router = Router();
 
 // Public routes
 router.get('/', getAllIdeas);
 router.get('/:id', getIdeaById);
-router.get('/details/:id', ideaDetails);
+router.get('/analytics/:id', ideaAnalatics); // get idea analytics
 
 // Authorized routes
 router.post('/', authorized, createIdea);
@@ -15,7 +15,7 @@ router.put('/:id', authorized, updateIdeaById); // (only by owner)
 router.delete('/:id', authorized, deleteIdeaById); // (only by owner)
 
 // votes
-router.post('/vote/:id', authorized, postVote); // upvote/downvote idea by id
+router.post('/vote/', authorized, postVote); // upvote/downvote idea by id (in body)
 router.get('/vote/:id', getVotes); // get idea's total votes
 
 export default router; 
