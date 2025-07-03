f1e9b33dc8ba5628e3e76e5f5d07e7be6e8f6ff0
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-07-04 01:30:23 +0300

    edit comments routes

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', getComments); // get all comments
 router.get('/idea/:id', getIdeaComments); // get all comments for one idea by its id, id = idea id
 
-router.post('/idea/', authorized, createComment);
+router.post('/idea/:id', authorized, createComment);
 router.delete('/:id', authorized, deleteComment); // Delete a comment by id
 
 // comment votes
